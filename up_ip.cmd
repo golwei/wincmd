91cc12ca946ad7e157bb267ff2aea6386b98d494
ZWLee <lizhiweimail@qq.com>
2019-03-30 01:43:14 +0800

    Update up_ip.cmd

--- up_ip.cmd
+++ up_ip.cmd
@@ -1,1 +1,3 @@
 (for /f "tokens=1 skip=2"  %%i in ('netsh interface ipv4 show interface') do (netsh interface ip set address name=%%i  source=dhcp))
+## 定时任务
+schtasks /create /sc hourly /mo 5 /sd 2019/04/20 /tn "My App" /tr .\aaa.bat
